7c2c3a3a14fc80389927af0cc872941497d042af
ChristopherLucas <clucas@fas.harvard.edu>
2014-04-14 10:36:46 -0400

    Renamed y-axis

--- R/plotMeans.R
+++ R/plotMeans.R
@@ -31,7 +31,7 @@ plotMeans <- function(frontierObject, dataset, frontierEstObject=NULL, zoom = NU
                  aes(x=Var1, y=value, colour=Var2)) +
                  geom_line() +
                  xlab("Number of Observations Pruned") +
-                 ylab("Standardized Mean Value") +
+                 ylab("Rescaled Mean Value") +
                  opts(legend.position="bottom") +
                  theme(legend.title=element_blank())
   }
