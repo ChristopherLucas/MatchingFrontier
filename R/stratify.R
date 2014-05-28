66be3a625938c47c0d79f9aae395985d508da12a
ChristopherLucas <clucas@fas.harvard.edu>
2014-05-28 16:47:32 -0400

    Delete stratify.R

--- R/stratify.R
+++
@@ -1,9 +0,0 @@
-# Takes a dataframe and returns a vector of length nrow(data), where
-# element i is strata for observation i. 
-stratify <- function (dataset){
-  xx <- apply(dataset, 1, function(x) paste(x, collapse = "\r"))
-  tab <- table(xx)
-  st <- names(tab)
-  strata <- match(xx,st)
-  return(strata)
-}
