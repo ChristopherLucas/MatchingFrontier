6994aa4354b3ceddf62e6bff18899cbb57b18cad
ChristopherLucas <clucas@fas.harvard.edu>
2014-05-28 16:46:28 -0400

    Delete frontierMultiPlot.R

--- R/frontierMultiPlot.R
+++
@@ -1,7 +0,0 @@
-frontierMultiPlot <- function(frontierObject, dataset, frontierEstObject=NULL, zoom = NULL, drop=NULL){
-  p1 <- plotFrontier(frontierObject, dataset, frontierEstObject, zoom, drop)
-  p2 <- plotEffects(frontierObject, dataset, frontierEstObject, zoom, drop)
-  p3 <- plotMeans(frontierObject, dataset, frontierEstObject, zoom, drop)
-  p.final <- multiPlot(p1, p2, p3, cols=1)
-  return(p.final)
-}
