66669b5bbf71f16732f1fc1bc0465142bc1f340e
ChristopherLucas <clucas@fas.harvard.edu>
2014-06-03 17:33:53 -0400

    Added onattach to NAMESPACE

--- R/MatchingFrontier.R
+++ R/MatchingFrontier.R
@@ -6,7 +6,26 @@
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
     msg <- gettextf("%s (%s %s)", dcf[, "Title"],
                     as.character(dcf[, "Version"]), dcf[, "Date"])
-    packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
+    packageStartupMessage("+----------------------------------------------------------------+
+|  Please refer to http://gking.harvard.edu/zelig for full       |
+|  documentation or help.zelig() for help with commands and      |
+|  models support by Zelig.                                      |
+|                                                                |
+|  Zelig project citations:                                      |
+|    Kosuke Imai, Gary King, and Olivia Lau.  (2009).            |
+|    ``Zelig: Everyone's Statistical Software,''                 |
+|    http://gking.harvard.edu/zelig                              |
+|   and                                                          |
+|    Kosuke Imai, Gary King, and Olivia Lau. (2008).             |
+|    ``Toward A Common Framework for Statistical Analysis        |
+|    and Development,'' Journal of Computational and             |
+|    Graphical Statistics, Vol. 17, No. 4 (December)             |
+|    pp. 892-913.                                                |
+|                                                                |
+|   To cite individual Zelig models, please use the citation     |
+|   format printed with each model run and in the documentation. |
++----------------------------------------------------------------+
+")
 }
 
 makeFrontier <- function(dataset, treatment, outcome, match.on, QOI, metric, ratio){
