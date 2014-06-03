57f77060ea8696d3678511111ebae05b750d2db1
ChristopherLucas <clucas@fas.harvard.edu>
2014-06-03 18:43:35 -0400

    Changed message

--- R/MatchingFrontier.R
+++ R/MatchingFrontier.R
@@ -4,8 +4,8 @@
 
 .onAttach <- function(lib, pkg){
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    version.msg <- paste('\n', 'Loading MatchingFrontier Version ', dcf[, 'Version'], sep = '')
-    cite.msg <- "To cite:\n\nKing, Gary, Christopher Lucas, and Richard Nielsen. 2014. \"Optimizing Balance and Sample Size in Matching Methods for Causal Inference.\" Working paper."
+    version.msg <- paste('\n\n', 'Loading MatchingFrontier Version ', dcf[, 'Version'], sep = '')
+    cite.msg <- "To cite:\nKing, Gary, Christopher Lucas, and Richard Nielsen. 2014. \"Optimizing Balance and Sample Size in Matching Methods for Causal Inference.\" Working paper."
     
     msg <- paste(version.msg, cite.msg, sep = '\n\n')
     packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
