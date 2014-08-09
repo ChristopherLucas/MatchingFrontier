3578f841e98c56832d23ccefb564a4a155535321
ChristopherLucas <clucas@fas.harvard.edu>
2014-08-08 23:30:32 -0400

    Edited zzz

--- package/R/zzz.R
+++ package/R/zzz.R
@@ -3,7 +3,7 @@
 
     version.msg <- paste('\n', 'Loading MatchingFrontier Version ', dcf[, 'Version'], sep = '')
 
-    cite.msg <- "King, Gary, Christopher Lucas, and Richard Nielsen. 2014. \"Optimizing Balance and Sample Size in Matching Methods for Causal Inference.\" Working paper."
+    cite.msg <- "King, Gary, Christopher Lucas, and Richard Nielsen. \"Optimizing Balance and Sample Size in Matching Methods for Causal Inference.\" Working paper (2014)."
     cite.msg <- paste(strwrap(cite.msg), collapse = "\n")
 
     bib.msg <- "@article{King14,\n\ttitle={Optimizing Balance and Sample Size in Matching Methods for Causal Inference},\n\tauthor={King, Gary and Lucas, Christopher and Nielsen, Richard},\n\tjournal={Working Paper},\n\tyear={2014}\n}\n"
