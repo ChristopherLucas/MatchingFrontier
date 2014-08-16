f84d76061bdaf92ffbfabb2d0940c988d9e51707
ChristopherLucas <clucas@fas.harvard.edu>
2014-08-16 17:39:43 -0400

    Added some printed feedback to L1 frontier

--- package/R/L1FrontierSATT.R
+++ package/R/L1FrontierSATT.R
@@ -1,7 +1,9 @@
 L1FrontierSATT <-
-function(treatment, outcome, dataset, breaks){    
+function(treatment, outcome, dataset, breaks){
+    cat("Calculating L1 binnings...\n")
     match.on <- colnames(dataset)[!(colnames(dataset) %in% c(treatment, outcome))]
     binnings <- getBins(dataset, treatment, match.on, breaks)
+    cat("Calculating L1 frontier... This may take a few minutes...\n")
     frontier <- binsToFrontier(binnings)
     out <- list(
         frontier = frontier,
