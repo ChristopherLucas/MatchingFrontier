6311da5f01501a004ec5d733f4751c9893a85577
ChristopherLucas <clucas@fas.harvard.edu>
2014-09-10 13:53:48 -0400

    Fixed d

--- package/R/zzz.R
+++ package/R/zzz.R
@@ -2,11 +2,13 @@
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
 
     version.msg <- paste('\n', 'Loading MatchingFrontier Version ', dcf[, 'Version'], sep = '')
-
+    
     cite.msg <- paste("King, Gary, Christopher Lucas, and Richard Nielsen. \"MatchingFrontier: R Package for Computing the Matching Frontier.\" R package version ", dcf[, 'Version'], '.', sep = '')
     cite.msg <- paste(strwrap(cite.msg), collapse = "\n")
 
-    bib.msg <- "@article{King14,\n\ttitle={MatchingFrontier: R Package for Computing the Matching Frontier},\n\tauthor={King, Gary and Lucas, Christopher and Nielsen, Richard},\n\tyear={2014},\n\tnote={R package version 0.3.12}\n}\n"
+    version.ref <- paste('R package version', dcf[, 'Version'])
+    
+    bib.msg <- paste("@article{King14,\n\ttitle={MatchingFrontier: R Package for Computing the Matching Frontier},\n\tauthor={King, Gary and Lucas, Christopher and Nielsen, Richard},\n\tyear={2014},\n\tnote={", version.ref, "}\n}\n")
         
     cite.msg <- paste('## Citation ##\n', cite.msg, sep = '')
 
