e737a1771f1dff66ef447d7e9fd8bcac9733dfc9
ChristopherLucas <clucas@fas.harvard.edu>
2014-10-07 16:51:06 -0400

    Added mod dependence stuff

--- package/R/plotEstimates.R
+++ package/R/plotEstimates.R
@@ -5,26 +5,43 @@ function(estimates.object,
          main = 'Effects Plot',
          ...){
 
-    # Plot estimates
-    X <- estimates.object$Xs
-    plot(X, estimates.object$coefs, xlab = xlab, ylab = ylab, main = main, ...)
-    segments(X, unlist(lapply(estimates.object$CIs, function(x) x[1])),
-           X, unlist(lapply(estimates.object$CIs, function(x) x[2])),
-           col = 'gray75')
-    points(X, estimates.object$coefs, ...)
-
+    plot(estimates.object$Xs, estimates.object$coefs, xlab = xlab, ylab = ylab, main = main, ...)
+    
     # Plot model dependence
+    Xs <- c()
+    Ys <- list()
+
+    
     for(i in 1:length(estimates.object$mod.dependence)){
         x.base <- as.integer(names(estimates.object$mod.dependence[i]))
-        print(x.base)
-#        return(list(x = x.base + density(estimates.object$mod.dependence[[i]])$y * 100000, y = density(mahal.estimates$mod.dependence[[1]])$x))
-
-        Xs <- x.base + density(estimates.object$mod.dependence[[i]])$y * 100000
-        Ys <- density(estimates.object$mod.dependence[[i]])$x
-
-        print(range(Xs))
-        print(range(Ys))
-
-        points(Xs, Ys, type = 'l', lwd = 2)
+        Xs <- c(Xs, x.base)
+        Ys[[length(Ys) + 1]] <- quantile(estimates.object$mod.dependence[[i]], probs = c(0, 1))
     }
+    
+    Xs <- c(Xs, rev(Xs))
+    Ys <- c(unlist(lapply(Ys, function(x) x[1])),
+              rev(unlist(lapply(Ys, function(x) x[2]))))
+    
+    polygon(Xs,
+            Ys,
+            col = rgb(1, 0, 0, 0.4), lty = 2, lwd = 1, border = "red")
+
+    
+    # Plot estimates
+    points(estimates.object$Xs, estimates.object$coefs, type = 'l')
+
+    
+    points(estimates.object$Xs, unlist(lapply(estimates.object$CIs, function(x) x[1])), type = 'l')
+    points(estimates.object$Xs, unlist(lapply(estimates.object$CIs, function(x) x[2])), type = 'l')    
+    
+    ## polygon(c(estimates.object$Xs, rev(estimates.object$Xs)),
+    ##         c(unlist(lapply(estimates.object$CIs, function(x) x[1])),
+    ##           rev(unlist(lapply(estimates.object$CIs, function(x) x[2])))),
+    ##         col = rgb(0, 0, 1, .5),
+    ##         lty = 2, lwd = 1, border = "blue")
+            
+    ## segments(estimates.object$Xs, unlist(lapply(estimates.object$CIs, function(x) x[1])),
+    ##          estimates.object$Xs, unlist(lapply(estimates.object$CIs, function(x) x[2])),
+    ##          col = 'black')
+    points(estimates.object$Xs, estimates.object$coefs, ...)
 }
