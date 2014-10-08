1b87c80b6d90504308dd796de711de5a7681a6ed
ChristopherLucas <clucas@fas.harvard.edu>
2014-10-08 11:41:48 -0400

    Added means plot

--- package/R/plotFrontier.R
+++ package/R/plotFrontier.R
@@ -1,9 +1,9 @@
 plotFrontier <-
 function(frontier.object,
-                         xlab = 'Number of Observations Pruned',
-                         ylab = frontier.object$metric,
-                         main = 'Frontier Plot',
-                         ...){
+         xlab = 'Number of Observations Pruned',
+         ylab = frontier.object$metric,
+         main = 'Frontier Plot',
+         ...){
 
     plot(frontier.object$frontier$Xs, frontier.object$frontier$Ys,
          xlab = xlab,
