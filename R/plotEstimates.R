6747fa23230a56e7c240a54ad5e5656518ea3835
ChristopherLucas <clucas@fas.harvard.edu>
2015-02-03 13:21:31 -0500

    Added border

--- R/plotEstimates.R
+++ R/plotEstimates.R
@@ -8,10 +8,13 @@ function(estimates.object,
          line.col = rgb(102,0,0,255, maxColorValue=255),
          ...){
 
+    plot.xlims <- c(0, max(estimates.object$Xs, na.rm = TRUE))
+    plot.ylims <- c(min(estimates.object$coefs - estimates.object$mod.dependence, na.rm = TRUE),
+                    max(estimates.object$coefs + estimates.object$mod.dependence, na.rm = TRUE))
+    
     plot(1, type="n", main, xlab=xlab, ylab=ylab,
-         xlim = c(0, max(estimates.object$Xs)),
-         ylim = c(min(estimates.object$coefs - estimates.object$mod.dependence, na.rm = TRUE),
-             max(estimates.object$coefs + estimates.object$mod.dependence, na.rm = TRUE)))
+         xlim = plot.xlims,
+         ylim = plot.ylims)
     polygon(c(estimates.object$Xs, rev(estimates.object$Xs)),
             c(estimates.object$coefs - estimates.object$mod.dependence,
               rev(estimates.object$coefs + estimates.object$mod.dependence)),
