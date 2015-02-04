43b94430cb5e38f67bc05b9bd99e8f81be046021
ChristopherLucas <clucas@fas.harvard.edu>
2015-02-03 21:06:10 -0500

    Added axis limits option

--- R/plotEstimates.R
+++ R/plotEstimates.R
@@ -3,18 +3,24 @@ function(estimates.object,
          xlab = 'Number of Observations Pruned',
          ylab = 'Estimate',
          main = 'Effects Plot',
+         xlim = NULL,
+         ylim = NULL,
          mod.dependence.col = rgb(255,0,0,127, maxColorValue=255),
          mod.dependence.border.col = rgb(255,0,0,200, maxColorValue=255),
          line.col = rgb(102,0,0,255, maxColorValue=255),
          ...){
 
-    plot.xlims <- c(0, max(estimates.object$Xs, na.rm = TRUE))
-    plot.ylims <- c(min(estimates.object$coefs - estimates.object$mod.dependence, na.rm = TRUE),
-                    max(estimates.object$coefs + estimates.object$mod.dependence, na.rm = TRUE))
+    if(is.null(xlim)){
+        xlim <- c(0, max(estimates.object$Xs, na.rm = TRUE))
+    }
+    if(is.null(ylim)){
+        ylim <- c(min(estimates.object$coefs - estimates.object$mod.dependence, na.rm = TRUE),
+                  max(estimates.object$coefs + estimates.object$mod.dependence, na.rm = TRUE))
+    }
     
     plot(1, type="n", main, xlab=xlab, ylab=ylab,
-         xlim = plot.xlims,
-         ylim = plot.ylims)
+         xlim = xlim,
+         ylim = ylim)
     polygon(c(estimates.object$Xs, rev(estimates.object$Xs)),
             c(estimates.object$coefs - estimates.object$mod.dependence,
               rev(estimates.object$coefs + estimates.object$mod.dependence)),
