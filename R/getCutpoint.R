587f374910cb89f4e152966ff202abf79be05914
ChristopherLucas <clucas@fas.harvard.edu>
2015-02-21 10:10:58 -0500

    switched back to seg reg

--- R/getCutpoint.R
+++ R/getCutpoint.R
@@ -1,15 +1,15 @@
 getCutpoint <-
 function(dataset, base.form, cov){
-#    base.mod <- lm(base.form, data = dataset)
-#    mod.form <- as.formula(paste(as.character(base.form[2]),
-#                                 as.character(base.form[1]),
-#                                 cov))
-#    print(cov)
-#    print(mean(dataset[[cov]]))
-#    print(base.mod)
-#    seg.reg <- segmented(base.mod, seg.Z=mod.form[c(1,3)], psi = mean(dataset[[cov]]))
-#    print(seg.reg)
-#    cutpoint <- seg.reg$psi[2]
-    cutpoint <- mean(dataset[[cov]])
+    base.mod <- lm(base.form, data = dataset)
+    mod.form <- as.formula(paste(as.character(base.form[2]),
+                                 as.character(base.form[1]),
+                                 cov))
+    print(cov)
+    print(mean(dataset[[cov]]))
+    print(base.mod)
+    seg.reg <- segmented(base.mod, seg.Z=mod.form[c(1,3)], psi = mean(dataset[[cov]]))
+    print(seg.reg)
+    cutpoint <- seg.reg$psi[2]
+#    cutpoint <- mean(dataset[[cov]])
     return(cutpoint)
 }
