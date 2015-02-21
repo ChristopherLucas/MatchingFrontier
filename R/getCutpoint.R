40ef933b676221e0899dff4e419779960e71be64
ChristopherLucas <clucas@fas.harvard.edu>
2015-02-21 11:47:43 -0500

    switched to glm

--- R/getCutpoint.R
+++ R/getCutpoint.R
@@ -1,11 +1,9 @@
 getCutpoint <-
 function(dataset, base.form, cov){
-#    base.mod <- lm(base.form, data = dataset)
+    base.mod <- glm(base.form, data = dataset, family = 'binomial')
     mod.form <- as.formula(paste(as.character(base.form[2]),
                                  as.character(base.form[1]),
                                  cov))
-    print(mod.form)
-    base.mod <- lm(mod.form, data = dataset)
     print(cov)
     print(base.mod); print(mod.form[c(1,3)]); print(mean(dataset[[cov]]))
     seg.reg <- segmented(base.mod, seg.Z=mod.form[c(1,3)], psi = median(dataset[[cov]]))
