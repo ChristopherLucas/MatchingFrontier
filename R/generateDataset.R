7b105205ffc9ba0bfffeb5c2d196715746f26002
ChristopherLucas <clucas@fas.harvard.edu>
2015-02-22 16:13:55 -0500

    Fixed prob where two minimums

--- R/generateDataset.R
+++ R/generateDataset.R
@@ -1,7 +1,7 @@
 generateDataset <-
 function(frontier.object, N){
     X <- nrow(frontier.object$dataset) - frontier.object$frontier$Xs
-    ind <- which(abs(X - N) == min(abs(X - N)))
+    ind <- which(abs(X - N) == min(abs(X - N)))[1]
     this.dat.inds <- unlist(frontier.object$frontier$drop.order[ind:length(frontier.object$frontier$drop.order)])
     dataset <- frontier.object$dataset[this.dat.inds,]
 
