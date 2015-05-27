b283de361f9e0d6f043fe46f84c07837f9d3d928
ChristopherLucas <clucas@fas.harvard.edu>
2015-05-27 08:42:44 -0400

    just some pseudocode

--- R/modelDependence.R
+++ R/modelDependence.R
@@ -1,6 +1,15 @@
 modelDependence <-
-function(dataset, treatment, verbose = TRUE, seed = 1, cutpoints = NA){
-	
-	print("Hello, world.")
+function(dataset, treatment, verbose = TRUE, seed = 1, num.binnings, cutpoints = NA){
+    set.seet(seed)
+    for(i in 1:num.binnings){
+        # should export a #bins x #obs matrix (see cut() + outer())
+
+        # model dependence in bins with common support is width of bins,
+        # model dependence in bins without common support is num obs
+        dummy.mat <- # sample from possible bins and asign dummies
+
+        
+    }
+
     
 }
