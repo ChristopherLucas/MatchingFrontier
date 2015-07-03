de2fc498f84876602f953edf31d9680337781d87
ChristopherLucas <clucas@fas.harvard.edu>
2015-07-03 11:21:57 -0400

    Fixed keep.vars bug

--- R/L1FrontierSATT.R
+++ R/L1FrontierSATT.R
@@ -1,5 +1,5 @@
 L1FrontierSATT <-
-function(treatment, outcome, dataset, breaks, match.on){
+function(treatment, outcome, dataset, breaks, match.on, keep.vars){
     cat("Calculating L1 binnings...\n")
     binnings <- getBins(dataset, treatment, match.on, breaks)
     cat("Calculating L1 frontier... This may take a few minutes...\n")
@@ -13,7 +13,8 @@ function(treatment, outcome, dataset, breaks, match.on){
         metric = 'L1',
         ratio = 'fixed',
         dataset = dataset,
-        match.on = match.on
+        match.on = match.on,
+        keep.vars = keep.vars
         )
     return(out)
 }
--- R/MahalFrontierFSATT.R
+++ R/MahalFrontierFSATT.R
@@ -1,5 +1,5 @@
 MahalFrontierFSATT <-
-function(treatment, outcome, dataset, ratio, match.on){    
+function(treatment, outcome, dataset, ratio, match.on, keep.vars){    
     distance.mat <- calculateMdist(dataset, treatment, match.on)
     frontier <- distToFrontier(distance.mat)
     dataset$matched.to <- frontier$matched.to
@@ -12,7 +12,8 @@ function(treatment, outcome, dataset, ratio, match.on){
         metric = 'Mahal',
         ratio = ratio,
         dataset = dataset,
-        match.on = match.on
+        match.on = match.on,
+        keep.vars = keep.vars
         )
     return(out)
 }
