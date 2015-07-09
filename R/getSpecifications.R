60bac61bc7569564001e5035dc3fa4752822cc96
ChristopherLucas <clucas@fas.harvard.edu>
2015-07-09 10:25:26 -0400

    Specs same across frontier

---
+++ R/getSpecifications.R
@@ -0,0 +1,46 @@
+getSpecifications <- function(covariates, treatment, outcome, dataset, N){
+
+    specifications <- c()
+
+    for(i in 1:N){
+        covs <- sample(covariates, sample(1:length(covariates), 1))
+        
+        cov.polys <- c()
+        for(cov in covs){
+            if(length(unique(dataset[[cov]])) <= 3){
+                cov.polys <- c(cov.polys, cov)
+                next
+            }
+            cov.polys <- c(cov.polys, paste('poly(', cov, ',', sample(1:3, 1), ', raw = TRUE)', sep = ''))
+        }
+        
+                                        # Double interactions
+        if(length(covs) > 1){
+            possible.interactions <- combn(covs, 2, simplify = FALSE)
+            cov.cols <- sample(1:length(possible.interactions), sample(1:length(possible.interactions), 1))
+            cov.interactions <- c()
+            for(cov.ind in 1:length(cov.cols)){
+                this.interaction <- paste(possible.interactions[[cov.ind]], collapse = ':')
+                cov.interactions <- c(cov.interactions, this.interaction)
+            }
+            formula <- paste(outcome,
+                             '~',
+                             paste(treatment, '+'),
+                             paste(paste(cov.polys, collapse = ' + ')),
+                             '+',
+                             paste(paste(cov.interactions, collapse = ' + '))
+                             )
+        }else{
+            formula <- paste(outcome,
+                             '~',
+                             paste(treatment, '+'),
+                             paste(paste(cov.polys, collapse = ' + '))
+                             )
+        }
+        if(i == 1){
+            formula <- paste(outcome, '~',  treatment)
+        }
+        specifications <- c(specifications, formula)
+    }
+    return(specifications)
+}
--- R/modelDependence.R
+++ R/modelDependence.R
@@ -3,54 +3,31 @@ function(dataset,
          treatment,
          outcome,
          covariates = NULL,
-         model.dependence.ests = 100,
+         model.dependence.ests = NULL,
          verbose = TRUE,
-         ratio = 'fixed'){
+         ratio = 'fixed',
+         specifications = NULL){
     if(is.null(covariates)){
         covariates <- colnames(dataset)[!(colnames(dataset) %in% c(treatment, outcome, 'matched.to'))]
     }
     set.seed(1)
-    # Get points to estimate
 
+    if(is.null(mod.dependence.ests) & is.null(specifications)){
+        stop("Must specify either 'specifications' or 'mod.dependence.ests'.")
+    }
+    
+    if(is.null(specifications)){
+        specifications <- getSpecifications(covariates, treatment, outcome, dataset, model.dependence.ests)
+    }    
+    
+    if(length(specifications) != model.dependence.ests){
+        stop("'model.dependence.ests' must equal the length of 'specifications'.")
+    }
+    
     coef.dist <- c()
     for(k in 1:model.dependence.ests){
-        covs <- sample(covariates, sample(1:length(covariates), 1))
-        
-        cov.polys <- c()
-        for(cov in covs){
-            if(length(unique(dataset[[cov]])) <= 3){
-                cov.polys <- c(cov.polys, cov)
-                next
-            }
-            cov.polys <- c(cov.polys, paste('poly(', cov, ',', sample(1:3, 1), ', raw = TRUE)', sep = ''))
-        }
-        
-        # Double interactions
-        if(length(covs) > 1){
-            possible.interactions <- combn(covs, 2, simplify = FALSE)
-            cov.cols <- sample(1:length(possible.interactions), sample(1:length(possible.interactions), 1))
-            cov.interactions <- c()
-            for(cov.ind in 1:length(cov.cols)){
-                this.interaction <- paste(possible.interactions[[cov.ind]], collapse = ':')
-                cov.interactions <- c(cov.interactions, this.interaction)
-            }
-            formula <- paste(outcome,
-                             '~',
-                             paste(treatment, '+'),
-                             paste(paste(cov.polys, collapse = ' + ')),
-                             '+',
-                             paste(paste(cov.interactions, collapse = ' + '))
-                             )
-        }else{
-            formula <- paste(outcome,
-                             '~',
-                             paste(treatment, '+'),
-                             paste(paste(cov.polys, collapse = ' + '))
-                             )
-        }
-        if(k == 1){
-            formula <- paste(outcome, '~',  treatment)
-        }
+        if(k == 10){ print(k) }
+        formula <- specifications[k]
         # run model
         if(ratio == 'variable'){
             w <- makeWeights(dataset, treatment)
